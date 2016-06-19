33383ee8c834f11e0b1307eb167ca6fe6ad617fa
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2016-06-18 19:14:19 -0500

    Setup Twitter API, added retrofit service and client

---
+++ app/src/main/java/me/juancrg90/android/twitterclient/api/CustomTwitterApiClient.java
@@ -0,0 +1,17 @@
+package me.juancrg90.android.twitterclient.api;
+
+import com.twitter.sdk.android.core.Session;
+import com.twitter.sdk.android.core.TwitterApiClient;
+
+/**
+ * Created by juancrg90
+ */
+public class CustomTwitterApiClient extends TwitterApiClient{
+    public CustomTwitterApiClient(Session session) {
+        super(session);
+    }
+
+    public TimelineService getTimelineService() {
+        return getService(TimelineService.class);
+    }
+}
