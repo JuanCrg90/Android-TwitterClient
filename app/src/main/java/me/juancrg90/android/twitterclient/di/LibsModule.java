7a84082310fddcd1e6daf3cffe7c2989256d66f4
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2016-06-18 19:34:35 -0500

    Added Dependency injection module

---
+++ app/src/main/java/me/juancrg90/android/twitterclient/di/LibsModule.java
@@ -0,0 +1,58 @@
+package me.juancrg90.android.twitterclient.di;
+
+import android.support.v4.app.Fragment;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import me.juancrg90.android.twitterclient.lib.GlideImageLoader;
+import me.juancrg90.android.twitterclient.lib.GreenRobotEventBus;
+import me.juancrg90.android.twitterclient.lib.base.EventBus;
+import me.juancrg90.android.twitterclient.lib.base.ImageLoader;
+
+/**
+ * Created by juancrg90.
+ */
+
+@Module
+public class LibsModule {
+    private Fragment fragment;
+
+    public LibsModule(Fragment fragment) {
+        this.fragment = fragment;
+    }
+
+    @Provides
+    @Singleton
+    ImageLoader providesImageLoader(RequestManager requestManager) {
+        return new GlideImageLoader(requestManager);
+    }
+
+    @Provides
+    @Singleton
+    RequestManager providesRequestManager() {
+        return Glide.with(fragment);
+    }
+
+    @Provides
+    @Singleton
+    Fragment providesFragment() {
+        return this.fragment;
+    }
+
+    @Provides
+    @Singleton
+    EventBus providesEventBus(org.greenrobot.eventbus.EventBus eventBus) {
+        return new GreenRobotEventBus(eventBus);
+    }
+
+    @Provides
+    @Singleton
+    org.greenrobot.eventbus.EventBus providesLibraryEventBus() {
+        return org.greenrobot.eventbus.EventBus.getDefault();
+    }
+}
