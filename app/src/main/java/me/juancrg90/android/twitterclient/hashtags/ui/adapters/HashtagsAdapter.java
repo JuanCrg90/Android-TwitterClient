5603cd42ffce2c8efa6cd86801628be85c71a886
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2016-06-20 16:53:17 -0500

    Implemented Twitter Hashtags Adapter

--- app/src/main/java/me/juancrg90/android/twitterclient/hashtags/ui/adapters/HashtagsAdapter.java
+++ app/src/main/java/me/juancrg90/android/twitterclient/hashtags/ui/adapters/HashtagsAdapter.java
@@ -1,25 +1,24 @@
 package me.juancrg90.android.twitterclient.hashtags.ui.adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import me.juancrg90.android.twitterclient.R;
 import me.juancrg90.android.twitterclient.entities.Hashtag;
-import me.juancrg90.android.twitterclient.entities.Image;
 
 /**
  * Created by juancrg90.
  */
 
-//TODO REWRITE THIS ADAPTER FOR HASHTAGS
 public class HashtagsAdapter extends RecyclerView.Adapter<HashtagsAdapter.ViewHolder> {
     private List<Hashtag> dataset;
     private me.juancrg90.android.twitterclient.hashtags.ui.adapters.OnItemClickListener clickListener;
@@ -32,7 +31,7 @@ public class HashtagsAdapter extends RecyclerView.Adapter<HashtagsAdapter.ViewHo
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.content_images, parent, false);
-        return new ViewHolder(view);
+        return new ViewHolder(view, parent.getContext());
     }
 
     @Override
@@ -40,6 +39,7 @@ public class HashtagsAdapter extends RecyclerView.Adapter<HashtagsAdapter.ViewHo
         Hashtag tweet = dataset.get(position);
         holder.setOnClickListener(tweet, clickListener);
         holder.txtTweet.setText(tweet.getTweetText());
+        holder.setItems(tweet.getHashtags());
     }
 
     public void setItems(List<Hashtag> newItems) {
@@ -55,15 +55,32 @@ public class HashtagsAdapter extends RecyclerView.Adapter<HashtagsAdapter.ViewHo
     class ViewHolder extends RecyclerView.ViewHolder {
         @Bind(R.id.txtTweet)
         TextView txtTweet;
-        @Bind(R.id.imgMedia)
-        ImageView imgMedia;
+        @Bind(R.id.recyclerView)
+        RecyclerView recyclerView;
+
         private View view;
+        private HastagListAdapter adapter;
+        private ArrayList<String> items;
 
 
-        public ViewHolder(View itemView) {
+        public ViewHolder(View itemView, Context context) {
             super(itemView);
             ButterKnife.bind(this, itemView);
             this.view = itemView;
+
+            items = new ArrayList<String>();
+            adapter = new HastagListAdapter(items);
+
+            CustomGridLayoutManager layoutManager = new CustomGridLayoutManager(context, 3);
+
+            recyclerView.setLayoutManager(layoutManager);
+            recyclerView.setAdapter(adapter);
+        }
+
+        public void setItems(List<String> newItems) {
+            items.clear();
+            items.addAll(newItems);
+            adapter.notifyDataSetChanged();
         }
 
         public void setOnClickListener(final Hashtag image, final OnItemClickListener listener) {
